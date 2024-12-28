22d14255b928f7d0daddeb310165c1063217c872
Mantu Kumar <118504139+mantu01714@users.noreply.github.com>
2024-12-28 11:02:10 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,36 @@
+//Max Area of Island
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int dfs(vector<vector<int>>& grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0) return 0;
+    grid[i][j] = 0;
+    return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
+}
+
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int maxArea = 0;
+    for (int i = 0; i < grid.size(); ++i) {
+        for (int j = 0; j < grid[0].size(); ++j) {
+            if (grid[i][j] == 1) {
+                maxArea = max(maxArea, dfs(grid, i, j));
+            }
+        }
+    }
+    return maxArea;
+}
+
+int main() {
+    vector<vector<int>> grid = {
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}
+    };
+    cout << "Max Area of Island: " << maxAreaOfIsland(grid) << endl;
+    return 0;
+}
